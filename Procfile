1880aab2c38e90cf48e096520591fb33b1f6c6ad
Angel Astudillo Aguilar <xavieras1@gmail.com>
2021-11-06 19:07:03 +0100

    adding auto migrate and Location instead of category

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
+release: python manage.py migrate
 web: gunicorn menu_app.wsgi --log-file -
--- product/admin.py
+++ product/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Category, Product
+from .models import Location, Product
 
-admin.site.register(Category)
+admin.site.register(Location)
 admin.site.register(Product)
