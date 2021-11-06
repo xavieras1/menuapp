df07317a8424fa34bb6658d3f0c53f261d7d923d
Angel Astudillo Aguilar <xavieras1@gmail.com>
2021-11-06 18:21:28 +0100

    fixing product model importation in product views

--- product/views.py
+++ product/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-
+from .models import Product
 from .serializers import ProductSerializer
 
 class LatestProductsList(APIView):
