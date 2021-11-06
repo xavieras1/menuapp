486169df98fef8409a0b6454d5f7d8ed8bf1e9ef
Angel Astudillo Aguilar <xavieras1@gmail.com>
2021-11-06 20:44:52 +0100

    include product details view

--- product/views.py
+++ product/views.py
@@ -11,3 +11,15 @@ class LatestProductsList(APIView):
         products = Product.objects.all()[0:4]
         serializer = ProductSerializer(products, many=True)
         return Response(serializer.data)
+
+class ProductDetail(APIView):
+    def get_object(self, location_slug, product_slug):
+        try:
+            return Product.objects.filter(location_slug=location_slug).get(slug=product_slug)
+        except Product.DoesNotExist:
+            raise Http404
+    
+    def get(self, request, location_slug, product_slug, format=None):
+        product = self.get_object(location_slug, product_slug)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
