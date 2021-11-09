8da62b0069cf6e06333080ebca9280ebe570544a
Angel Astudillo Aguilar <xavieras1@gmail.com>
2021-11-09 11:10:27 +0100

    Meal model definition!

--- meal/models.py
+++ meal/models.py
@@ -1,3 +1,63 @@
+from io import BytesIO
+from PIL import Image
+
+from django.core.files import File
 from django.db import models
 
-# Create your models here.
+from product.models import Product
+
+
+class Meal(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField()
+    description = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to='uploads/', blank=True, null=True)
+    thumbnail = models.ImageField(upload_to='uploads/', blank=True, null=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('-date_added',)
+    
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return f'/{self.slug}/'
+    
+    def get_image(self):
+        if self.image:
+            return 'https://a-y-a-menu.herokuapp.com' + self.image.url
+        return ''
+    
+    def get_thumbnail(self):
+        if self.thumbnail:
+            return 'https://a-y-a-menu.herokuapp.com' + self.thumbnail.url
+        else:
+            if self.image:
+                self.thumbnail = self.make_thumbnail(self.image)
+                self.save()
+
+                return 'https://a-y-a-menu.herokuapp.com' + self.thumbnail.url
+            else:
+                return ''
+    
+    def make_thumbnail(self, image, size=(300, 200)):
+        img = Image.open(image)
+        img.convert('RGB')
+        img.thumbnail(size)
+
+        thumb_io = BytesIO()
+        img.save(thumb_io, 'JPEG', quality=85)
+
+        thumbnail = File(thumb_io, name=image.name)
+
+        return thumbnail
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Meal, related_name='items', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, related_name='items', on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    measure = models.CharField(max_length=255)
+
+    def __str__(self):
+        return '%s' % self.id
