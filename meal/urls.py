a8c2163a7437bf932d00922967e4966a9e8f432e
Angel Astudillo Aguilar <xavieras1@gmail.com>
2021-11-09 17:24:29 +0100

    changing url to lastest meals

--- meal/urls.py
+++ meal/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from meal import views
 
 urlpatterns = [
-    path('latest-products/', views.LatestMealsList.as_view()),
+    path('latest-meals/', views.LatestMealsList.as_view()),
     #path('products/search/', views.search),
     #path('products/<slug:location_slug>/<slug:product_slug>/', views.ProductDetail.as_view()),
     #path('products/<slug:location_slug>/', views.LocationDetail.as_view()),
