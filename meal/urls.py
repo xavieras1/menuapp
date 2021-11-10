c5088bff7715bda9745706bb8e5b8fa1dd6f8feb
Angel Astudillo Aguilar <xavieras1@gmail.com>
2021-11-10 12:45:12 +0100

    creation of meal detail view

--- meal/urls.py
+++ meal/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('latest-meals/', views.LatestMealsList.as_view()),
     #path('products/search/', views.search),
     #path('products/<slug:location_slug>/<slug:product_slug>/', views.ProductDetail.as_view()),
-    #path('products/<slug:location_slug>/', views.LocationDetail.as_view()),
+    path('meals/<slug:meal_slug>/', views.MealDetail.as_view()),
 ]
--- meal/views.py
+++ meal/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import Http404
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -10,4 +11,16 @@ class LatestMealsList(APIView):
     def get(self, request, format=None):
         meals = Meal.objects.all()[0:4]
         serializer = MealSerializer(meals, many=True)
+        return Response(serializer.data)
+
+class MealDetail(APIView):
+    def get_object(self, meal_slug):
+        try:
+            return Meal.objects.get(slug=meal_slug)
+        except Meal.DoesNotExist:
+            raise Http404
+    
+    def get(self, request, meal_slug, format=None):
+        meal = self.get_object(meal_slug)
+        serializer = MealSerializer(meal)
         return Response(serializer.data)
