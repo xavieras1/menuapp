ddaa945169c6cfd9c428909bf6f3faf7d33429af
Angel Astudillo Aguilar <xavieras1@gmail.com>
2021-11-16 00:25:39 +0100

    update orderlists working 100%

--- itemslist/views.py
+++ itemslist/views.py
@@ -21,32 +21,11 @@ class ItemsList(APIView):
     def post(self, request, format=None):
         print(request.data)
         order_list = OrderList.objects.filter(type=request.data['type'])
-        print(order_list)
         order_items = ListItem.objects.filter(list=order_list[0])#.delete()
         order_items.delete()
-        print(order_items)
-        print(len(order_items))
-        #for item_data in request.data['items']:
-        #    a=ListItem.objects.create(order=order_list, **item_data)
-        #    print(a)
-        #serializer = OrderListSerializer(order_list[0])
-        #print(serializer.data)
         serializer = OrderListSerializer(order_list[0], data=request.data)
         serializer.create(request.data)
-        #serializer.is_valid(raise_exception=True)
-        
-        #print(serializer.data)
-        #serializer = ListItemSerializer(request.data['items'], many=True)
-        #serializer.save(list=order_list)
-        #print(serializer2.data)
-        #serializer.data['items'] = serializer2.data
-        #print(serializer.data)
         if serializer.is_valid():
-            print('valido')
-            #print(serializer.data)
-            
-            #serializer.save(list=order_list[0])
-            #serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
