b0935ed6685cffa9dc989d2214484fd8eadfa7e0
Angel Astudillo Aguilar <xavieras1@gmail.com>
2021-11-15 22:18:35 +0100

    update orderlists beggining v38

--- itemslist/serializers.py
+++ itemslist/serializers.py
@@ -38,6 +38,6 @@ class OrderListSerializer(serializers.ModelSerializer):
         for item_data in items_data:
             product_data = item_data.pop('product')
             product = Product.objects.get(pk=product_data['id'])
-            ListItem.objects.create(order=order,product=product, **item_data)
+            ListItem.objects.create(list=order,product=product, **item_data)
             
         return order
