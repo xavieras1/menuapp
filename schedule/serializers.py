cb7eeb27f779dc47a40e0715c4fdc82ec5d14684
Angel Astudillo Aguilar <xavieras1@gmail.com>
2021-11-16 21:59:03 +0100

    changing list field to schedule field in schedule.serializers

--- schedule/serializers.py
+++ schedule/serializers.py
@@ -11,7 +11,7 @@ class ScheduleItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = ScheduleItem
         fields = (
-            "list",
+            "schedule",
             "meal",
             "quantity",
             "person",
